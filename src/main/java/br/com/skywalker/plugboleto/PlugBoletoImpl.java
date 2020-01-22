a6555dacbbbb869f45973f6cc63872eecb206d02
Yehoshua Oliveira <ioxua.oliveira@gmail.com>
2020-01-22 11:02:17 -0300

    feat: implements PlugBoletoImpl

--- src/main/java/br/com/skywalker/plugboleto/PlugBoletoImpl.java
+++ src/main/java/br/com/skywalker/plugboleto/PlugBoletoImpl.java
@@ -1,13 +1,39 @@
 package br.com.skywalker.plugboleto;
 
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
 
-@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 class PlugBoletoImpl extends PlugBoleto {
 
     private final Credentials credentials;
     private final Environment environment;
+
+    private final Retrofit retrofitInstance;
+
+    PlugBoletoImpl(Credentials credentials, Environment environment) {
+        this.credentials = credentials;
+        this.environment = environment;
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor((chain) -> {
+                    Request newRequest = chain.request().newBuilder()
+                            .addHeader("Content-Type", "application/json")
+                            .addHeader("cnpj-sh", credentials.getShCnpj())
+                            .addHeader("token-sh", credentials.getShToken())
+                            .build();
+
+                    return chain.proceed(newRequest);
+                })
+                .build();
+
+        retrofitInstance = new Retrofit.Builder()
+                .baseUrl(environment.getBaseUrl())
+                .addConverterFactory(JacksonConverterFactory.create())
+                .client(client)
+                .build();
+    }
 
     // *****************************************************************************
 
