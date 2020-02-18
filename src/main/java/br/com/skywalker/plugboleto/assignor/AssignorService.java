6c87e868799383dfb91996617b427e882f5a3115
Yehoshua Oliveira <ioxua.oliveira@gmail.com>
2020-02-18 16:45:35 -0300

    feat(temp): adds a post example for Marcelo

--- src/main/java/br/com/skywalker/plugboleto/assignor/AssignorService.java
+++ src/main/java/br/com/skywalker/plugboleto/assignor/AssignorService.java
@@ -1,11 +1,16 @@
 package br.com.skywalker.plugboleto.assignor;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
 
 interface AssignorService {
 
     @GET(Endpoints.BASE)
     Call<AssignorPagedResponse> findAll();
+    @POST("/")
+    Call<AssignorPagedResponse> sendSomething(@Body Object a, @Header("X-Auth") String asd);
 
 }
